4ea5f097b9379d36b109b9c8f7511ce4a20b150e
Ziad-Sallam <ziadsallam1234@gmail.com>
2023-12-21 12:03:30 +0200

    bug fixed

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(){
 
     settings main_menu;
 
-    main_menu = mainmenu(main_menu);
+    main_menu = mainmenu();
 
     int r, c;
     if(main_menu.diff ==1) {r=5; c=5;}
@@ -26,23 +26,62 @@ int main(){
 
     }
 
-    int r1, r2, c1, c2;
+    
 
     while(1)
     {
-        system("cls");
+        char r1c, r2c, c1c, c2c,ch;
+        int r1 =0,r2=0,c1=0,c2=0;
+        
+        //system("cls");
         printlogo();
         printGrid(r, c, grid);
         
         printf("Make a move!\n");
-        scanf("%d %d %d %d", &r1, &r2, &c1, &c2);
+        
+        ch =getchar();
+        while(scanf("%1c %1c %1c %1c",  &r1c,&r2c,&c1c, &c2c) != 4){
+                printf("Invalid input! Make another move!\n");
+            }
+            
+        
+        r1 = (r1c)-48;
+        c2 = (c2c)-48;
+        c1 = (c1c)-48;
+        r2 = (r2c)-48;
+       
         r1 = r1*2-2; r2 = r2*2-2; c1 = c1*2-2; c2 = c2*2-2;
-
+        
         while (validate(r1, r2, c1, c2) != 1){
             printf("Invalid input! Make another move!\n");
-            scanf("%d %d %d %d", &r1, &r2, &c1, &c2);
+            ch =getchar();
+            
+            while(scanf("%1c %1c %1c %1c",  &r1c,&r2c,&c1c, &c2c) !=4){
+                printf("Invalid input! Make another move!\n");
+            }
+            
+            
+            r1 = (r1c)-48;
+            c2 = (c2c)-48;
+            c1 = (c1c)-48;
+            r2 = (r2c)-48;
+            r1 = r1*2-2; r2 = r2*2-2; c1 = c1*2-2; c2 = c2*2-2;
         }
 
+        if (c1 > c2)
+        {
+            c1 = c1 ^ c2;
+            c2 = c1 ^ c2;
+            c1 = c1 ^ c2;
+        }
+
+        if (r1 > r2)
+        {
+            r1 = r1 ^ r2;
+            r2 = r1 ^ r2;
+            r1 = r1 ^ r2;
+        }
+         
         if (validate(r1, r2, c1, c2) == 1){
             if (r1 == r2 && grid[r1][c1 +1] != '_'){
                 grid[r1][c1 +1] = '_';
